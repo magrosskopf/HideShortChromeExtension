5386d65a15cbcdb6c42dd298e39dd39db9cb9cd5
Marius <grosskopf81@gmail.com>
2024-11-16 17:19:30 +0100

    initial commit

---
+++ content.js
@@ -0,0 +1,25 @@
+(function() {
+    function hideShorts() {
+      const shortsSelectors = [
+        'ytd-rich-grid-media[is-shorts]',
+        'a[href*="/shorts/"]',
+        'a[title="Shorts"]',
+        'ytd-thumbnail[href*="/shorts/"]',
+        '.ShortsLockupViewModelHostOutsideMetadataSubhead',
+        'hortsLockupViewModelHostOutsideMetadata',
+        'h2[aria-label="Shorts"]',
+        'h2#title.style-scope.ytd-rich-shelf-renderer',    
+      ];
+  
+      shortsSelectors.forEach(selector => {
+        const elements = document.querySelectorAll(selector);
+        elements.forEach(el => el.style.display = 'none');
+      });
+    }
+  
+    hideShorts();
+  
+    const observer = new MutationObserver(hideShorts);
+    observer.observe(document.body, { childList: true, subtree: true });
+  })();
+  
